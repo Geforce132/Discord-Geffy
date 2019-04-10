2873b36bdeb07e2605985af43d15f0db84fb53c4
Geforce <Geforce132@gmail.com>
2019-04-09 21:47:01 -0500

    Updated version and gitignore

--- src/net/geforce/geffy/main/Geffy.java
+++ src/net/geforce/geffy/main/Geffy.java
@@ -8,7 +8,9 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.util.DiscordException;
 
-public class Geffy {	
+public class Geffy {
+	
+	public static final String VERSION = "1.3.0";
 	
 	/**
 	 * Geffy's Discord client instance.
